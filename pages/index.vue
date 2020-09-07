d83dbe1c72814074dc1c8086b2d7266e939b8c04
Precious Ibeagi <preciousaang@gmail.com>
2020-09-07 12:38:00 +0100

    centralized header

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid>
     <v-card class="d-flex justify-center align-center" height="500px">
-      <h1>
+      <h1 class="text-center  ">
         We are Building Something Great
       </h1>
     </v-card>
