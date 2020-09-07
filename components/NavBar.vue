36a16b46dbddef4e2eaee8d315f73171294a8f8e
Precious Ibeagi <preciousaang@gmail.com>
2020-09-07 12:34:33 +0100

    Added Navbar and placeholder for homepage

---
+++ components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <v-app-bar dense flat app>
+      <v-toolbar-title>DexHub Creations</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -16,12 +16,12 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s - ' + process.env.APP_NAME,
+    title: process.env.APP_NAME || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.APP_NAME || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -70,7 +70,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
