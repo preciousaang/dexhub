6c0426e8bbf32fe211afd5a9918e7acb20c0ae4a
Precious Ibeagi <preciousaang@gmail.com>
2020-10-04 03:43:43 +0100

    Reworking the homepage

--- pages/index.vue
+++ pages/index.vue
@@ -31,208 +31,164 @@
     </v-carousel>
     <v-container fluid>
       <v-row>
-        <v-col data-aos="fade-left" data-aos-duration="3000" cols="12" lg="6" align-self="center">
-          <h1 class="primary-story text-center">
-            DexHub Creations is a brand
-          </h1>
+        <v-col align-self="center" cols="12" lg="6">
+          <v-img src="/networking-png-picture.png" />
         </v-col>
-        <v-col data-aos="flip-down-left" data-aos-duration="3000" cols="12" lg="6">
-          <h2 class="primary-story mb-3">
+        <v-col align-self="center" cols="12" lg="6">
+          <h2 class="mb-5">
             Our Community
           </h2>
-          <v-divider />
-          <p class="story text--secondary">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          <p class="subtitle-1">
+            t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
           </p>
-          <v-btn class="mt-3" large color="info" outlined>
-            Learn More
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col align-self="center" cols="12" lg="6">
+          <h2 class="mb-5 ml-lg-10">
+            Projects We Love
+          </h2>
+          <p class="subtitle-1 ml-lg-10">
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. <br><br>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+          </p>
+          <v-btn class="ml-lg-10" style="border: 2px solid #96bbcf" rounded color="#2d779f" dark>
+            Discover
           </v-btn>
         </v-col>
-      </v-row>
-      <div data-aos="fade-up" data-aos-duration="3000">
-        <v-row>
-          <v-col class="pa-15" cols="12" lg="6">
-            <h2 class="primary-story mb-3">
-              Explore the projects we love
-            </h2>
-            <v-divider />
-            <p class="story text--secondary">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-            </p>
-            <v-btn class="mt-3" large color="info" outlined>
-              Learn More
-            </v-btn>
-          </v-col>
-          <v-col cols="12" lg="6" align-self="center">
-            <v-row data-aos="flip-left" data-aos-easing="linear" data-aos-duration="7000" align="center" justify="center">
-              <v-col
-                cols="12"
-                lg="3"
-                sm="3"
-                md="3"
-                xs="3"
-              >
-                <v-img class="ma-5" src="/orange_circle.png" />
-                <h4 class="text-center text--secondary">
-                  Art
-                </h4>
-              </v-col>
-              <v-col cols="12" lg="3" md="3" sm="3" xs="3">
-                <v-img class="ma-5" src="/orange_circle.png" />
-                <h4 class="text-center text--secondary">
-                  Art
-                </h4>
-              </v-col>
-              <v-col cols="12" lg="3" sm="3" xs="3" md="3">
-                <v-img class="ma-5" src="/orange_circle.png" />
-                <h4 class="text-center text--secondary">
-                  Art
-                </h4>
-              </v-col>
-              <v-col cols="12" lg="3" sm="3" xs="3" md="3">
-                <v-img class="ma-5" src="/orange_circle.png" />
-                <h4 class="text-center text--secondary">
-                  Art
-                </h4>
-              </v-col>
-              <v-col cols="12" lg="3" sm="3" xs="3" md="3">
-                <v-img class="ma-5" src="/orange_circle.png" />
-                <h4 class="text-center text--secondary">
-                  Art
-                </h4>
-              </v-col>
-              <v-col cols="12" lg="3" sm="3" xs="3" md="3">
-                <v-img class="ma-5" src="/orange_circle.png" />
-                <h4 class="text-center text--secondary">
-                  Art
-                </h4>
-              </v-col>
-              <v-col cols="12" lg="3" sm="3" xs="3" md="3">
-                <v-img class="ma-5" src="/orange_circle.png" />
-                <h4 class="text-center text--secondary">
-                  Art
-                </h4>
-              </v-col>
-              <v-col cols="12" lg="3" sm="3" xs="3" md="3">
-                <v-img class="ma-5" src="/orange_circle.png" />
-                <h4 class="text-center text--secondary">
-                  Art
-                </h4>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-      </div>
-      <v-container>
-        <v-row justify="center" align="center">
-          <v-col align-self="center" cols="12" lg="3" data-aos="zoom-in-right" data-aos-duration="3000">
-            <h3 class="primary-story">
-              Upcoming Projects
-            </h3>
-            <v-divider />
-            <p class="story text--secondary">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            </p>
-          </v-col>
-          <v-col cols="12" lg="9" data-aos="zoom-in-left" data-aos-duration="10000">
-            <v-slide-group center-active show-arrows>
-              <v-slide-item>
-                <v-card width="300" class="ma-4" elevation="4" height="100%">
-                  <v-img class="ma-5" height="50%" src="/grey.jpg" />
-                  <v-card-title>
-                    Demo Title
-                  </v-card-title>
-                  <v-card-text>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                  </v-card-text>
-                </v-card>
-              </v-slide-item>
-              <v-slide-item>
-                <v-card width="300" elevation="4" class="ma-4" height="100%">
-                  <v-img class="ma-5" height="50%" src="/grey.jpg" />
-                  <v-card-title>
-                    Demo Title
-                  </v-card-title>
-                  <v-card-text>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                  </v-card-text>
-                </v-card>
-              </v-slide-item>
-              <v-slide-item>
-                <v-card width="300" elevation="4" class="ma-4" height="100%">
-                  <v-img class="ma-5" height="50%" src="/grey.jpg" />
-                  <v-card-title>
-                    Demo Title
-                  </v-card-title>
-                  <v-card-text>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                  </v-card-text>
-                </v-card>
-              </v-slide-item>
-              <v-slide-item>
-                <v-card width="300" elevation="4" class="ma-4" height="100%">
-                  <v-img class="ma-5" height="50%" src="/grey.jpg" />
-                  <v-card-title>
-                    Demo Title
-                  </v-card-title>
-                  <v-card-text>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                  </v-card-text>
-                </v-card>
-              </v-slide-item>
-            </v-slide-group>
-          </v-col>
-        </v-row>
-      </v-container>
-      <v-divider />
-      <h1 class="text-center primary-story mt-5">
-        What Our Cients Say
-      </h1>
-      <p style="color: #df5f20; font-weight: bold;" class="text-center">
-        ________
-      </p>
-      <v-row justify="center" align="center">
-        <v-col cols="12" lg="10">
-          <v-carousel hide-delimiter-background delimiter-icon="mdi-minus">
-            <v-carousel-item>
-              <v-sheet height="100%">
-                <v-row justify="center" align="center" height="center" class="fill-height">
-                  <div>
-                    <v-img src="/earth-1.jpg" width="150" class="mx-auto rounded-circle mb-5" height="150" />
-                    <p class="subtitle-1 px-10 text-center">
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                    </p>
-                    <h1 class="text-center">
-                      Alan Shore
-                    </h1>
-                    <p class="text-center subtitle-1">
-                      Fictional Character
-                    </p>
-                  </div>
-                </v-row>
-              </v-sheet>
-            </v-carousel-item>
-            <v-carousel-item>
-              <v-sheet height="100%">
-                <v-row justify="center" align="center" height="center" class="fill-height">
-                  <div>
-                    <v-img src="/earth-1.jpg" width="150" class="mx-auto rounded-circle mb-5" height="150" />
-                    <p class="subtitle-1 px-10 text-center">
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                    </p>
-                    <h1 class="text-center">
-                      Alan Shore
-                    </h1>
-                    <p class="text-center subtitle-1">
-                      Fictional Character
-                    </p>
-                  </div>
-                </v-row>
-              </v-sheet>
-            </v-carousel-item>
-          </v-carousel>
+        <v-col align-self="center" cols="12" lg="6">
+          <v-row>
+            <v-col
+              align-self="center"
+              cols="12"
+              lg="4"
+              xs="6"
+              md="4"
+              sm="6"
+            >
+              <v-img width="60%" class="mx-auto" src="/eagle.png" />
+              <p class="subtitle-2 text-center">
+                Eagle Tech
+              </p>
+            </v-col>
+            <v-col
+              align-self="center"
+              cols="12"
+              lg="4"
+              xs="6"
+              md="4"
+              sm="6"
+            >
+              <v-img width="60%" class="mx-auto" src="/orange_circle-2.png" />
+              <p class="subtitle-2 text-center">
+                One Tech
+              </p>
+            </v-col>
+            <v-col
+              align-self="center"
+              cols="12"
+              lg="4"
+              xs="6"
+              md="4"
+              sm="6"
+            >
+              <v-img width="60%" class="mx-auto" src="/eagle.png" />
+              <p class="subtitle-2 text-center">
+                Eagle Tech
+              </p>
+            </v-col>
+            <v-col
+              align-self="center"
+              cols="12"
+              lg="4"
+              xs="6"
+              md="4"
+              sm="6"
+            >
+              <v-img width="60%" class="mx-auto" src="/orange_circle-2.png" />
+              <p class="subtitle-2 text-center">
+                One Tech
+              </p>
+            </v-col>
+            <v-col
+              align-self="center"
+              cols="12"
+              lg="4"
+              xs="6"
+              md="4"
+              sm="6"
+            >
+              <v-img width="60%" class="mx-auto" src="/eagle.png" />
+              <p class="subtitle-2 text-center">
+                Eagle Tech
+              </p>
+            </v-col>
+            <v-col
+              align-self="center"
+              cols="12"
+              lg="4"
+              xs="6"
+              md="4"
+              sm="6"
+            >
+              <v-img width="60%" class="mx-auto" src="/orange_circle-2.png" />
+              <p class="subtitle-2 text-center">
+                One Tech
+              </p>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
+    </v-container>
+    <!-- Change This Nonsense -->
+    <v-divider />
+    <h1 class="text-center primary-story mt-5">
+      What Our Cients Say
+    </h1>
+    <p style="color: #df5f20; font-weight: bold;" class="text-center">
+      ________
+    </p>
+    <v-row justify="center" align="center">
+      <v-col cols="12" lg="10">
+        <v-carousel hide-delimiter-background delimiter-icon="mdi-minus">
+          <v-carousel-item>
+            <v-sheet height="100%">
+              <v-row justify="center" align="center" height="center" class="fill-height">
+                <div>
+                  <v-img src="/earth-1.jpg" width="150" class="mx-auto rounded-circle mb-5" height="150" />
+                  <p class="subtitle-1 px-10 text-center">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                  </p>
+                  <h1 class="text-center">
+                    Alan Shore
+                  </h1>
+                  <p class="text-center subtitle-1">
+                    Fictional Character
+                  </p>
+                </div>
+              </v-row>
+            </v-sheet>
+          </v-carousel-item>
+          <v-carousel-item>
+            <v-sheet height="100%">
+              <v-row justify="center" align="center" height="center" class="fill-height">
+                <div>
+                  <v-img src="/earth-1.jpg" width="150" class="mx-auto rounded-circle mb-5" height="150" />
+                  <p class="subtitle-1 px-10 text-center">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                  </p>
+                  <h1 class="text-center">
+                    Alan Shore
+                  </h1>
+                  <p class="text-center subtitle-1">
+                    Fictional Character
+                  </p>
+                </div>
+              </v-row>
+            </v-sheet>
+          </v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
     </v-container>
     <v-row>
       <v-col align-self="center" cols="12" lg="12">
